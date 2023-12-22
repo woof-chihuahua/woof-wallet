54a23c371b6bc7511ed9649e24b58719be3ba5b0
atmoner <contact.atmoner@gmail.com>
2023-12-22 19:29:09 +0100

    Fix only huahua buy

--- src/views/AddHuahuaView.vue
+++ src/views/AddHuahuaView.vue
@@ -5,7 +5,7 @@
     </div>
   </h1> 
   <div style="display: flex; justify-content: center;">
-    <iframe src="https://app.kado.money/?apiKey=9e654c7f-1014-4a81-8f6b-025d9e7e0d84&network=Chihuahua&onRevCurrency=HUAHUA&product=BUY&productList=BUY" width="480" height="720" style="border: 0px;"></iframe>
+    <iframe src="https://app.kado.money/?apiKey=9e654c7f-1014-4a81-8f6b-025d9e7e0d84&network=Chihuahua&networkList=Chihuahua&onRevCurrency=HUAHUA&cryptoList=HUAHUA&product=BUY&productList=BUY" width="480" height="720" style="border: 0px;"></iframe>
   </div>  
 </template>
  
